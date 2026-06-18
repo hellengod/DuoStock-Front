217c06a085ab739c2d6a3bd905f8673ec5391b40
Vinicassao <viniciusconta1@outlook.com.br>
2026-06-17 21:33:28 -0300

    feat: adicionando a página dashboard e criando a rota para navegação

---
+++ src/pages/DashBoard.vue
@@ -0,0 +1,2 @@
+<h1>Estou na Dashboard</h1>
+<p>Testando...</p>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Já estou adicionando as rotas do menu porque estou usando routerlink ;)
+import DashBoardView from '@/pages/DashBoard.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashBoardView
+    }
+  ],
 })
 
 export default router
